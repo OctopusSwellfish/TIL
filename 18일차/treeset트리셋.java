018694a964877de7a50dfe54f9e7f930b28adce1
OctopusSwellfish <010-2648-8131@daum.net>
2021-04-20 22:43:19 +0900

    :feat: treeset to Array in java

---
+++ 18일차/treeset트리셋.java
@@ -0,0 +1,15 @@
+TreeSet<String> ts = new TreeSet<String>(new Comparator<String>() {
+				@Override
+				public int compare(String o1, String o2) {
+					return o2.compareTo(o1);
+				}
+			});
+
+
+//로 만들고
+
+//string[] 으로 뺄떄는
+
+String[] result = ts.toArray(new String[ts.size()]);
+
+//지금같은 경우는 스트링 정려할때 큰 게 앞으로오는것이다.
